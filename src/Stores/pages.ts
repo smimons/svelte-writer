27d5ffa521709d253de8b80d59839acf634701c7
Simon Caine <scaine@caci.co.uk>
2021-02-17 12:13:25 +0000

    removed firebase

--- src/Stores/pages.ts
+++ src/Stores/pages.ts
@@ -1,10 +1,7 @@
 import { writable } from "svelte/store";
-// import { db } from "../Utils/firebase";
 import type { Page } from "../Utils/interfaces";
 
 const DEFAULT_PAGES: Record<string, Page> = {}
-
-// const PAGE_COLLECTION = db.collection('pages');
 
 const localPages: Record<string, Page> = JSON.parse(localStorage.getItem("pages")) ?? {};
 
@@ -31,30 +28,5 @@ const updatePage = (page: Page) => {
 }
 
 const addNewPage = updatePage;
-
-// const getAllPages = () => PAGE_COLLECTION.get().then(snapshot => {
-//     snapshot.forEach(doc => {
-//         const page = doc.data();
-//         update(pages => ({ ...pages, [page.timestamp]: page }))
-//     })
-// });
-
-// const deletePage = async (timestamp: string) => {
-//     update(pages => {
-//         delete pages[timestamp]
-//         return pages;
-//     });
-//     await PAGE_COLLECTION.doc(timestamp).delete();
-// }
-
-// const updatePage = async (page: Page) => {
-//     update(pages => ({ ...pages, [page.timestamp]: page }))
-//     await PAGE_COLLECTION.doc(page.timestamp).update({ body: page.body })
-// }
-
-// const addNewPage = async (page: Page) => {
-//     update(pages => ({ ...pages, [page.timestamp]: page }));
-//     await PAGE_COLLECTION.doc(page.timestamp).set(page);
-// }
 
 export default { subscribe, updatePage, addNewPage, deletePage, getAllPages }
